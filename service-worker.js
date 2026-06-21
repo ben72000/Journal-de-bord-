9ebc75e8cc8bbf94904ccd0130d1d1f8ee19b660
ben72000 <132427668+ben72000@users.noreply.github.com>
2026-06-21 09:30:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    séparément, dans le stockage de ton appareil). Il ne met en cache que
    l'app elle-même. */
 
-const CACHE = 'compagnon-v2';
+const CACHE = 'compagnon-v3';
 const ASSETS = [
   './',
   './index.html',
